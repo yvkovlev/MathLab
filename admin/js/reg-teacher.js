edf40d22f0007dcd667716ae497ced375d07e784
Yakovlev Denis <barkov777@gmail.com>
2017-04-02 19:32:07 +0600

    Added validation in AP

--- admin/js/reg-teacher.js
+++ admin/js/reg-teacher.js
@@ -5,21 +5,23 @@ $(document).ready(function() {
       type: 'put',
       data: {fullname: $('#fullname').val(), email: $('#email').val(), password: $('#password').val(), phone: $('#tel').val(), sex: $('#sex option:selected').text(), subject: $('#subject option:selected').text()},
       success: function(response){
-        if (response == "Success") {
-          $(".alerts").html("<div class='alert alert-success alert-dismissable'>" +
-                              "<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>" +
-                              "<strong>Готово!</strong> Учитель успешно зарегистрирован." +
-                            "</div>");
-          $('#fullname').val("");
-          $('#email').val("");
-          $('#password').val("");
-          $('#tel').val("");
-        }
-        else {
-          $(".alerts").html("<div class='alert alert-danger alert-dismissable'>" +
-                              "<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>" +
-                              "<strong>Ошибка!</strong> Пользователь с таким E-mail уже зарегистрирован." +
-                            "</div>");
+        if (!$("#reg-teacher").hasClass("disabled")) {
+          if (response == "Success") {
+            $(".alerts").html("<div class='alert alert-success alert-dismissable'>" +
+                                "<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>" +
+                                "<strong>Готово!</strong> Учитель успешно зарегистрирован." +
+                              "</div>");
+            $('#fullname').val("");
+            $('#email').val("");
+            $('#password').val("");
+            $('#tel').val("");
+          }
+          else {
+            $(".alerts").html("<div class='alert alert-danger alert-dismissable'>" +
+                                "<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>" +
+                                "<strong>Ошибка!</strong> Пользователь с таким E-mail уже зарегистрирован." +
+                              "</div>");
+          }
         }
       }
     });
